ad2c72dab1f9c12a5527d8ed7c034d4b9e8a28d1
DeborahSoanavalona <deborah.razafindrabezandrina@esti.mg>
2025-06-13 10:49:22 +0300

    exo 3

--- Exercices.java
+++ Exercices.java
@@ -4,14 +4,14 @@ public class Exercices{
 
 	public static void main(String[]  args){
 		// EXERCICE 1
-		System.out.println("Bonjour\n");
+		System.out.println("Bonjour");
 
 
 
 		// EXERCICE 2
-		System.out.println("Bienvenue dans la calculatrice Java !!");
+		System.out.println("Bienvenue dans la calculatrice Java !!\n");
 
-		System.out.println("Veuillez entrer un deux nombres : \n");
+		System.out.println("Veuillez entrer deux nombres : ");
 		Scanner A = new Scanner(System.in); // premier nombre
 		Scanner B = new Scanner(System.in); // deuxième nombre
 
@@ -28,20 +28,22 @@ public class Exercices{
 
 		System.out.println("L'addition de " + a + " et de " + b + " est égale à " + add);
 		System.out.println("Le produit de " + a + " et de " + b + " est égal à " + produit);
-		System.out.println("La division de " + a + " par " + b + " est égale à " + div);
+		System.out.println("La division de " + a + " par " + b + " est égale à " + div + "\n");
 
 
 		// EXERCICE 3
-		 System.out.println("Échange de ces deux entiers avec JAVA \n");
+		 System.out.println("Échange de ces deux entiers avec JAVA");
 		System.out.println("Vous avez entré a = "+ a + " et b = "+ b + "\n" );
 		int x = a;
 		int c = b;
 		int d = x;
-		System.out.println("Ces deux entiers évhangés font a = "+ c + "et b = " + d + "\n" );
+		System.out.println("Ces deux entiers échangés font a = "+ c + " et b = " + d + "\n" );
 
 
 		// EXERCICE 4
 		System.out.println("Regardons si vos entrées sont pairs ou impairs ou divisibles par 3");
+		/* parité */
+		System.out.println((c % 2 == 0)? c + " est un nombre pair" :c +  " est impair");
 		
 	};
 }
